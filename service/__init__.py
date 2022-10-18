3c4d0cc7210505834b92da1216d4614a0cd1a9a7
ashaychangwani <ashaychangwani@gmail.com>
2022-10-18 15:37:34 -0400

    Fixed cyclic import

--- service/__init__.py
+++ service/__init__.py
@@ -14,10 +14,10 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 # Dependencies require we import the routes AFTER the Flask app is created
-# pylint: disable=wrong-import-position, wrong-import-order
-from service import routes, models  # noqa: E402, E261
+# pylint: disable=wrong-import-position, wrong-import-order, cyclic-import
+from service import routes, models  # noqa: E402, E261, F401
 # pylint: disable=wrong-import-position
-from service.common import error_handlers, cli_commands  # noqa: F401, E402
+from service.common import error_handlers, cli_commands  # noqa: F401, E402, F401
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
